3d6faf8a884b3cea08cc837b3846dc847aa9a836
unknown <lucassamaniego447@gmail.com>
2023-09-24 03:45:26 -0300

    Se corrige baner / se finalizan las secciones

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("hola estoy aca");
+const imgBanner = document.getElementById("imgBanner");
+const images = ["utils/banner1.png","utils/banner2.png","utils/banner3.png"];
+let currentIndex = 0;
+
+function changeImage(){
+    console.log("hola")
+    imgBanner.classList.add("hidden");
+    setTimeout(()=>{
+        imgBanner.src = images[currentIndex];
+        imgBanner.classList.remove("hidden");
+        currentIndex = (currentIndex + 1) % images.length;
+    }, 1000)
+}
+
+changeImage();
+setInterval(changeImage,10000)
